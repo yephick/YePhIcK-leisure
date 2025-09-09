407c7a1ac9ade59d256abba2e3be545f302691f9
YePhIcK <yephick@hotmail.com>
2025-09-09 20:31:06 +0200

    Add UI elements for runlock app

--- Win/runlock/MainWindow.xaml.cpp
+++ Win/runlock/MainWindow.xaml.cpp
@@ -4,14 +4,102 @@
 #include "MainWindow.g.cpp"
 #endif
 
+#include <thread>
+#include <winrt/Windows.ApplicationModel.DataTransfer.h>
+
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace winrt::runlock::implementation
 {
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        Loaded({ this, &MainWindow::Window_Loaded });
+    }
+
+    void MainWindow::Window_Loaded(IInspectable const&, RoutedEventArgs const&)
+    {
+        uint32_t cores = std::thread::hardware_concurrency();
+        if (cores == 0) { cores = 1; }
+        for (uint32_t i = 1; i <= cores; ++i)
+        {
+            CpuCoresComboBox().Items().Append(box_value(i));
+        }
+        CpuCoresComboBox().SelectedIndex(0);
+    }
+
+    void MainWindow::BrowseArchive_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        // TODO: Implement file picker for RAR files
+    }
+
+    void MainWindow::Archive_DragOver(IInspectable const&, DragEventArgs const& e)
+    {
+        e.AcceptedOperation(Windows::ApplicationModel::DataTransfer::DataPackageOperation::Copy);
+    }
+
+    void MainWindow::Archive_Drop(IInspectable const&, DragEventArgs const&)
+    {
+        // TODO: Handle dropped RAR file
+    }
+
+    void MainWindow::PasswordRules_DragOver(IInspectable const&, DragEventArgs const& e)
+    {
+        e.AcceptedOperation(Windows::ApplicationModel::DataTransfer::DataPackageOperation::Copy);
+    }
+
+    void MainWindow::PasswordRules_Drop(IInspectable const&, DragEventArgs const&)
+    {
+        // TODO: Populate password rules from dropped text file
+    }
+
+    void MainWindow::GeneratePasswords_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        // TODO: Generate list of passwords
+        CancelGenerateButton().IsEnabled(true);
+        GenerateButton().IsEnabled(false);
+    }
+
+    void MainWindow::CancelGenerate_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        // TODO: Cancel password generation
+        CancelGenerateButton().IsEnabled(false);
+        GenerateButton().IsEnabled(true);
+    }
+
+    void MainWindow::UnlockButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        switch (m_unlockState)
+        {
+        case UnlockState::Stopped:
+            m_unlockState = UnlockState::Running;
+            UnlockButton().Content(box_value(L"Pause"));
+            StatusText().Text(L"Running...");
+            break;
+        case UnlockState::Running:
+            m_unlockState = UnlockState::Paused;
+            UnlockButton().Content(box_value(L"Resume"));
+            StatusText().Text(L"Paused");
+            break;
+        case UnlockState::Paused:
+            m_unlockState = UnlockState::Stopped;
+            UnlockButton().Content(box_value(L"Start"));
+            StatusText().Text(L"Stopped");
+            break;
+        }
+    }
+
+    void MainWindow::SaveProject_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        // TODO: Save project settings
+    }
+
+    void MainWindow::LoadProject_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        // TODO: Load project settings
+    }
+
     int32_t MainWindow::MyProperty()
     {
         throw hresult_not_implemented();
--- Win/runlock/MainWindow.xaml.h
+++ Win/runlock/MainWindow.xaml.h
@@ -6,14 +6,26 @@ namespace winrt::runlock::implementation
 {
     struct MainWindow : MainWindowT<MainWindow>
     {
-        MainWindow()
-        {
-            // Xaml objects should not call InitializeComponent during construction.
-            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
-        }
+        MainWindow();
 
         int32_t MyProperty();
         void MyProperty(int32_t value);
+
+        void BrowseArchive_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void Archive_DragOver(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::DragEventArgs const& args);
+        void Archive_Drop(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::DragEventArgs const& args);
+        void PasswordRules_DragOver(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::DragEventArgs const& args);
+        void PasswordRules_Drop(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::DragEventArgs const& args);
+        void GeneratePasswords_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void CancelGenerate_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void UnlockButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void SaveProject_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void LoadProject_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void Window_Loaded(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+
+    private:
+        enum class UnlockState { Stopped, Running, Paused };
+        UnlockState m_unlockState{ UnlockState::Stopped };
     };
 }
 
