561593c3eaad512d704c9c35699305ad3897b609
YePhIcK <yephick@hotmail.com>
2026-02-18 17:34:05 +0100

    Fix storyline crash from background duration callback

--- Win/llvc/MainWindow.xaml.h
+++ Win/llvc/MainWindow.xaml.h
@@ -10,18 +10,32 @@ struct MainWindow: MainWindowT<MainWindow>{
     void StartButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
     void PauseButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
     void StopButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    void TimelineZoomSlider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& args);
+    void TimelineCanvas_PointerPressed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& args);
     void Window_DragOver(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::DragEventArgs const& args);
     winrt::Windows::Foundation::IAsyncAction Window_Drop(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::DragEventArgs const& args);
     void OnClosed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::WindowEventArgs const& args);
+    void OnNaturalDurationChanged(winrt::Windows::Media::Playback::MediaPlaybackSession const& sender, winrt::Windows::Foundation::IInspectable const& args);
+    void OnPositionTimerTick(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::Foundation::IInspectable const& args);
 
 private:
     winrt::Windows::Media::Playback::MediaPlayer m_player{nullptr};
+    winrt::Windows::Storage::StorageFile m_loadedFile{nullptr};
+    winrt::Windows::Media::Playback::MediaPlaybackSession::NaturalDurationChanged_revoker m_naturalDurationChangedRevoker{};
+    winrt::Microsoft::UI::Xaml::DispatcherTimer m_positionTimer{nullptr};
+    double m_timelineDurationSeconds{0};
+    std::uint64_t m_timelineRenderVersion{0};
+    bool m_isClosing{false};
     HWND GetWindowHandle() const;
 
 private:
     void RestoreWindowPlacement();
     void SaveWindowPlacement() const;
     winrt::Windows::Foundation::IAsyncAction LoadVideoFileAsync(winrt::Windows::Storage::StorageFile const& file);
+    winrt::fire_and_forget RenderTimelineAsync();
+    void UpdateTimelineCursorFromPlayback();
+    static winrt::Windows::Foundation::TimeSpan SecondsToTimeSpan(double seconds);
+    static bool IsRectVisibleOnAnyMonitor(RECT const& rect);
 };
 
 }
